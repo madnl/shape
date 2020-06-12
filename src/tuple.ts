62afc828d2c7d119e50cfbaefcd06108c10346b3
Marius Danila <mdanila@netflix.com>
2020-06-11 20:39:57 -0700

    checkpoint

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,37 +1,24 @@
 import { Shape } from '.';
 import { Mismatch, mismatch, isMismatch, nestedMismatch } from './core';
 
-export function tuple<T1, T2>(shape1: Shape<T1>, shape2: Shape<T2>): Shape<[T1, T2]>;
-export function tuple<T1, T2, T3>(
-  shape1: Shape<T1>,
-  shape2: Shape<T2>,
-  shape3: Shape<T3>
-): Shape<[T1, T2, T3]>;
-export function tuple<T1, T2, T3, T4>(
-  shape1: Shape<T1>,
-  shape2: Shape<T2>,
-  shape3: Shape<T3>,
-  shape4: Shape<T4>
-): Shape<[T1, T2, T3, T4]>;
-export function tuple<T1, T2, T3, T4, T5>(
-  shape1: Shape<T1>,
-  shape2: Shape<T2>,
-  shape3: Shape<T3>,
-  shape4: Shape<T4>,
-  shape5: Shape<T5>
-): Shape<[T1, T2, T3, T4, T5]>;
-export function tuple<T>(...itemTypes: Shape<T>[]): Shape<T[]> {
-  return new Tuple(itemTypes);
+type TupleOfShapes = readonly Shape<unknown>[];
+
+type TypeFromShapeTuple<ArrayT extends TupleOfShapes> = Shape<{ [K in keyof ArrayT]: ArrayT[K] }>;
+
+export function tuple<ArrayT extends TupleOfShapes>(
+  ...itemTypes: ArrayT
+): Shape<TypeFromShapeTuple<ArrayT>> {
+  return new TupleShape(itemTypes);
 }
 
-class Tuple<T> implements Shape<T[]> {
-  public readonly itemTypes: readonly Shape<T>[];
+class TupleShape<ArrayT extends TupleOfShapes> implements Shape<TypeFromShapeTuple<ArrayT>> {
+  public readonly itemTypes: ArrayT;
 
-  constructor(itemTypes: readonly Shape<T>[]) {
+  constructor(itemTypes: ArrayT) {
     this.itemTypes = itemTypes;
   }
 
-  check(value: unknown): T[] | Mismatch {
+  check(value: unknown): TypeFromShapeTuple<ArrayT> | Mismatch {
     if (!Array.isArray(value) || value.length !== this.itemTypes.length) {
       return mismatch(this, value);
     }
@@ -43,6 +30,6 @@ class Tuple<T> implements Shape<T[]> {
         return nestedMismatch(`[${i}]`, result);
       }
     }
-    return value;
+    return (value as unknown) as TypeFromShapeTuple<ArrayT>;
   }
 }
